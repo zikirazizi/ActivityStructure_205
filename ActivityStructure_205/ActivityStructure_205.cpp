577d401732084e091f0aac712a4ea4b2a3dcebbe
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-04-03 14:46:48 +0700

    Membuat fungsi input

--- ActivityStructure_205/ActivityStructure_205.cpp
+++ ActivityStructure_205/ActivityStructure_205.cpp
@@ -9,5 +9,26 @@ struct Mahasiswa {
 	int umur;
 };
 
+int main()
+{
+	Mahasiswa mhs1, mhs2;
 
+	mhs1.nim = "20220140205";
+	mhs1.nama = "Zikir";
+	mhs1.alamat = "Jogja";
+	mhs1.umur = 19;
+	
+	cout << "Masukkan NIM : ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs2.Alamat;
+	cout << "Masukkan Umur : ";
+	cin >> mhs2.umur;
+	
+
+	cout << "\nNim :
+
+};
 
